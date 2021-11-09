1bc2dba2c5adfba86c7567b75eda9a9e7ac8d9e8
Crystal Lee <cyurilee@gmail.com>
2021-11-09 01:22:40 -0500

    Iterations from Usability Test Insights

--- js/index.js
+++ js/index.js
@@ -5,3 +5,20 @@ function hideMenu(){
 function showMenu(){
     navLinks.style.right ="0";
 }
+
+var mybutton = document.getElementById("scrollTopBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
